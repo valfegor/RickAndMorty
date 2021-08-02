00f1f7a9808a297cfc8f296bd4f5472f132162e8
Anthony Ahumada <locopala99@gmail.com>
2021-08-02 08:13:00 +0200

    practicing api

---
+++ assets/js/rickandmortyapi.js
@@ -0,0 +1,23 @@
+//URL API.
+//las constantes que nunca van a cambiar de valor se declaran con MAYUSCULA.
+const API = "https://rickandmortyapi.com/api/character";
+
+
+//obtener resultados de la API.
+
+const getData = ()=>{
+
+}
+
+
+//dibujar cards de personajes.
+
+const llenarDatos = () =>{
+
+}
+
+//paginacion.
+
+const paginacion = () => {
+    
+}
