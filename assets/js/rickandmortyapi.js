546c3562796547f6f2dcfef3b18a53f80e2f800f
Anthony Ahumada <locopala99@gmail.com>
2021-08-02 08:56:56 +0200

    More api

--- assets/js/rickandmortyapi.js
+++ assets/js/rickandmortyapi.js
@@ -4,20 +4,42 @@ const API = "https://rickandmortyapi.com/api/character";
 
 
 //obtener resultados de la API.
-
-const getData = ()=>{
-
+//Una funcion puede recibir un paremetro
+//aqui se puede colocar cualquier nombre sobre el parametro
+//el fetch trae una promesa , es decir 
+//aqui se pasa la url , es la peticion
+//si devuelve algo se usa el then
+const getData = (url)=>{
+    return fetch(url)
+    //necesito un json
+    .then((response)=> response.json())
+    //con el json ubico los datos que necesito
+    .then((json)=>{
+        llenarDatos(json.resuls),paginacion(json.info);
+    })
+    //el error
+    .catch((error)=>{
+        console.log("Error" , error);
+    });
 }
 
 
 //dibujar cards de personajes.
-
-const llenarDatos = () =>{
-
+//aqui obtengo lo que saque de json.result
+const llenarDatos = (data) =>{
+    
 }
 
 //paginacion.
 
 const paginacion = () => {
-    
+
 }
+
+
+//fetch permite consumir los valores.
+
+//se ejecuta la api.
+//se envia la url como paramero.
+//de esta manera se envia.
+getData(API);
